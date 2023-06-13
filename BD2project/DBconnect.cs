26863b217d22823e27ea1ace3e5d0eaa9a4dbb6a
Administrator <Administrator@WINSERV01>
2023-06-13 11:41:34 +0200

    Rough console app with working add person to database functionality. Add project files.

--- BD2project/DBconnect.cs
+++ BD2project/DBconnect.cs
@@ -12,13 +12,12 @@ namespace FamilyTree
 
 		public DBconnect(String srvName)
 		{
-			connectionString = string.Format(@"Data Source=" + srvName + " ; Initial Catalog=BD2; INTEGRATED SECURIT=SSPI;");
-
+			connectionString = string.Format(@"Data Source=" + srvName + " ; Initial Catalog=bd2project; User ID=sa;Password=123");
 		}
 
 		public bool addPerson(string fname, string lname)
 		{
-			string sqlcmd = "insert into Person(firstname, lastname, gen) values ('" + fname + "', '" + lname + "', '/');";
+            string sqlcmd = "insert into Person(firstname, lastname, gen) values ('" + fname + "', '" + lname + "', '/');";
 			using (SqlConnection cnn = new SqlConnection(connectionString))
 			{
 				SqlCommand cmd = new SqlCommand(sqlcmd, cnn);
@@ -33,10 +32,74 @@ namespace FamilyTree
 				{
 					Console.WriteLine(ex.Message);
 				}
-				return false;
 			}
 			return false;
 		}
+
+		public void printPersonData(DataSet dataSet)
+        {
+			Console.WriteLine("id:\t" + dataSet.Tables[0].Rows[0]["id"].ToString());
+			Console.WriteLine("first name:\t" + dataSet.Tables[0].Rows[0]["firstname"].ToString());
+			Console.WriteLine("lastname:\t" + dataSet.Tables[0].Rows[0]["lastname"].ToString());
+			Console.WriteLine("date of birth:\t" + dataSet.Tables[0].Rows[0]["dateofbirth"].ToString());
+			Console.WriteLine("gender:\t" + dataSet.Tables[0].Rows[0]["gender"].ToString());
+		}
+
+		public bool isIdInDb(int id)
+        {
+			string sqlcmd = "select id, firstname, lastname, dateofbirth, gender from Person where id=" + id + ";";
+			SqlDataAdapter adapter = null;
+			DataSet dataSet = null;
+
+			try
+				{
+                adapter = new SqlDataAdapter(sqlcmd, connectionString);
+				dataSet = new DataSet();
+				adapter.Fill(dataSet);
+				printPersonData(dataSet);
+				return true;
+
+			}
+			catch (SqlException ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			
+			return false;
+		}
+
+		public DataSet getPersonData(int id)
+        {
+			string sqlcmd = "select id, firstname, lastname, dateofbirth, gender from Person where id=" + id + ";";
+			SqlDataAdapter adapter = null;
+			DataSet dataSet = null;
+
+			try
+			{
+				adapter = new SqlDataAdapter(sqlcmd, connectionString);
+				dataSet = new DataSet();
+				adapter.Fill(dataSet);
+				return dataSet;
+			}
+			catch (SqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return dataSet;
+		}
+
+		public string getName(int id)
+		{
+			DataSet person = getPersonData(id);
+			string ret = person.Tables[0].Rows[0]["firstname"] + " " + person.Tables[0].Rows[0]["lastname"];
+			return ret;
+
+		}
+
+		public void modifyRelationship(int id1, int id2, string rel)
+        {
+
+        }
 
 	}
 }
--- BD2project/FamilyTreeUI.cs
+++ BD2project/FamilyTreeUI.cs
@@ -10,34 +10,195 @@ namespace FamilyTree
 		public GenealogyUI()
 		{
 			db = new DBconnect("WINSERV01");
+
+			choicesMenu();
+			
+
+			
+
+
+		}
+
+		public void choicesMenu()
+        {
 			Console.WriteLine("Welcome to your own family tree builder! Let's start with choosing frome these options:\n");
 			Console.WriteLine("1. Add person\n");
 
+			globalChoicesListing();
+			Console.WriteLine("1. Add person");
+			Console.WriteLine("2. Modify relationship between two people");
+			Console.WriteLine("3. Get parents of a person");
+			Console.WriteLine("4. Get siblings of a person");
+			Console.WriteLine("5. Get children of a person");
+			Console.WriteLine("6. Remove a person");
 
+			Console.WriteLine("Now enter the number of the otion you want to choose:");
 			string inp = Console.ReadLine();
-
+			optionToQuit(inp);
+			
 			switch (inp)
 			{
 				case "1":
-					Console.WriteLine("Create a person\n");
-					addPerson();
+					Console.WriteLine("\n-> Add a person");
+					addPerson(false);
+					break;
+				case "2":
+					Console.WriteLine("\n-> Modify relationship between two people");
+					modifyRelationship();
+					break;
+				case "3":
+					Console.WriteLine("\n-> Get parents of a person");
+					break;
+				case "4":
 					break;
 				default:
+					Console.WriteLine("Incorrect input, try again!");
+					choicesMenu();
 					break;
 
 			}
 		}
 
-		void addPerson()
+		public void globalChoicesListing()
+        {
+			Console.WriteLine("To quit the program input 'x' whenever you are asked for input");
+			Console.WriteLine("To see the main menu input 'p' whenever you are asked for input\n");
+		}
+
+		public void optionToQuit(string opt)
+        {
+			switch (opt)
+			{
+				case "x":
+					Console.WriteLine("Goodbye!");
+					Environment.Exit(0);
+					break;
+				case "p":
+					choicesMenu();
+					break;
+				default:
+					break;
+			}
+        }
+
+		void addPerson(bool relMod)
 		{
 			Console.WriteLine("CREATE A PERSON:\nENTER FIRST NAME:\n");
 			string fname = Console.ReadLine();
+			optionToQuit(fname);
 			Console.WriteLine("First name: " + fname);
 			Console.WriteLine("\nENTER LAST NAME:\n");
 			string lname = Console.ReadLine();
+			optionToQuit(lname);
 			Console.WriteLine("Last name: " + lname);
-
+			db.addPerson(fname, lname);
+			choicesMenu();
 		}
+		
+		void printRelationshipTypes()
+        {
+			Console.WriteLine("'p' - a is a parent of b");
+			Console.WriteLine("'c' - a is a child of b");
+		}
+		string getRelationship()
+        {
+			string rel = "";
+			bool getCorrectRel = false;
+			while (!getCorrectRel)
+			{
+				Console.WriteLine("\nENTER TYPE OF RELATIONSHIP:\n");
+				printRelationshipTypes();
+				rel = Console.ReadLine();
+				optionToQuit(rel);
+                switch (rel)
+                {
+					case "p":
+						getCorrectRel = true;
+						break;
+					case "c":
+						getCorrectRel = true;
+						break;
+					default:
+						Console.WriteLine("Incorrect input, try again!");
+						break;
+				}
+			}
+			return rel;
+		}
+
+		void modifyRelationship()
+        {
+			int id1 = -1;
+			int id2 = -1;
+			bool getCorrectRel = false;
+			Console.WriteLine("Enter the id number of the first person");
+            while (!getCorrectRel)
+            {
+				id1 = Int32.Parse(Console.ReadLine());
+				Console.WriteLine(id1);
+                if (db.isIdInDb(id1))
+                {
+					Console.WriteLine("Is this the person you were thinking of? if yes type 'y', if no type 'n'");
+					getCorrectRel = confirmation();
+                }
+            }
+			getCorrectRel = false;
+			Console.WriteLine("Enter the id number of the second person");
+			while (!getCorrectRel)
+			{
+				id2 = Int32.Parse(Console.ReadLine());
+				Console.WriteLine(id2);
+				if (db.isIdInDb(id2))
+				{
+					if (id1 != id2)
+					{
+						Console.WriteLine("Is this the person you were thinking of? if yes type 'y', if no type 'n'");
+						getCorrectRel = confirmation();
+					}
+                    else
+                    {
+						Console.WriteLine("You can only modify a relatinship between two people! try again!");
+					}
+
+				}
+			}
+			string rel = establishRelationship(id1, id2);
+			db.modifyRelationship(id1, id2, rel);
+			
+		}
+
+		string establishRelationship(int id1, int id2)
+        {
+			string person1 = db.getName(id1);
+			string person2 = db.getName(id2);
+
+			Console.WriteLine("Who is " + person1 + " to " + person2 + "?");
+			printRelationshipTypes();
+			string inp = getRelationship();
+			return inp;
+        }
+
+		bool confirmation()
+        {
+			
+			string inp = Console.ReadLine();
+
+			switch(inp)
+            {
+				case "y":
+					return true;
+				case "n":
+					return false;
+				default:
+					Console.WriteLine("Incorrect input, try again!");
+					break;
+            }
+			return false;
+		}
+		void getParents()
+        {
+			Console.WriteLine();
+        }
 
 	}
 
